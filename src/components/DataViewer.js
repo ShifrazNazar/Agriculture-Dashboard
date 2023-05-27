dfa68919cb1065f3a786a5b433423ab1d16d4a9b
Shifraz Nazar <shifraznazar@gmail.com>
2023-05-28 00:15:39 +0800

    Update data viewer filter options

--- src/components/DataViewer.js
+++ src/components/DataViewer.js
@@ -18,7 +18,10 @@ const DataEntry = () => {
           setFilteredValues([]);
         }
       } catch (error) {
-        console.error(`Error retrieving ${selectedCollection} data from Firestore:`, error);
+        console.error(
+          `Error retrieving ${selectedCollection} data from Firestore:`,
+          error
+        );
       }
     };
 
@@ -45,13 +48,32 @@ const DataEntry = () => {
         >
           <option value="">Select Product</option>
           <option value="BiologyBalance">Biology Balance</option>
-          <option value="Tree">Tree</option>
+          <option value="Canola">Canola</option>
+          <option value="CationBalance">Cation Balance</option>
+          <option value="ChineseCabbage">Chinese Cabbage</option>
+          <option value="Corn">Corn</option>
+          <option value="Cotton">Cotton</option>
+          <option value="Crop">Crop</option>
+          <option value="Flowers">Flowers</option>
+          <option value="Forage">Forage</option>
+          <option value="NutrientBalance">Nutrient Balance</option>
+          <option value="OilPalm">Oil Palm</option>
+          <option value="Other">Other</option>
           <option value="PaddyGrain">Paddy Grain</option>
+          <option value="Peanut">Peanut</option>
+          <option value="Poppy">Poppy</option>
+          <option value="Sorghum">Sorghum</option>
+          <option value="Sugarcane">Sugarcane</option>
+          <option value="Tea">Tea</option>
+          <option value="Tobacco">Tobacco</option>
+          <option value="Tree">Tree</option>
+          <option value="Wheat">Wheat</option>
+          <option value="WildCabbage">Wild Cabbage</option>
         </select>
       </div>
       {filteredValues.length > 0 && (
         <div>
-          <h3 className="text-lg font-bold mt-4">Filtered Values</h3>
+          <h3 className="mt-4 text-lg font-bold">Filtered Values</h3>
           <table className="mt-2 border-collapse border border-gray-300">
             <thead>
               <tr>
@@ -59,7 +81,7 @@ const DataEntry = () => {
                 {Object.keys(filteredValues[0]).map((key) => (
                   <th
                     key={key}
-                    className="border border-gray-300 px-4 py-2 font-bold text-sm text-gray-700"
+                    className="border border-gray-300 px-4 py-2 text-sm font-bold text-gray-700"
                   >
                     {key}
                   </th>
